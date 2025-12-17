a6d482f940c383cffe260f114643c63ad9f0f8c6
Eric Jumper <ericjumper@pm.me>
2025-12-17 16:21:13 -0600

    Swap active and secondary Invidious instances

--- worker.js
+++ worker.js
@@ -698,10 +698,10 @@ const INVIDIOUS_INSTANCES = {
 };
 
 // Main instance: used for feed links (with embed mode and parameters)
-const ACTIVE_INVIDIOUS_INSTANCE = INVIDIOUS_INSTANCES.yewtu;
+const ACTIVE_INVIDIOUS_INSTANCE = INVIDIOUS_INSTANCES.nadeko;
 
 // Secondary instance: used for description fallback link (simple watch page)
-const SECONDARY_INVIDIOUS_INSTANCE = INVIDIOUS_INSTANCES.nadeko;
+const SECONDARY_INVIDIOUS_INSTANCE = INVIDIOUS_INSTANCES.yewtu;
 
 export default {
   async fetch(request, env) {
